437a319f01b13edf0e62c14875cf6071726cb609
Kartik Shah <shahkartikr@gmail.com>
2011-01-23 23:00:48 -0600

    Changes done.

--- src/rss/stat.clj
+++ src/rss/stat.clj
@@ -1,9 +1,5 @@
 (ns rss.stat)
 
-
-; -> rss-item.get-title-in-list -> filter-words -> group-words -> count-words -> sort-words
-; -> keywords -> find-in-title
-
 (def keywords ["Clojure" "Groovy" "Python"])
 
 (defn find-items [rss-item-list keyword]
@@ -12,11 +8,6 @@
 (defn find-items-keywords [rss-item-list keyword-list]
   (flatten (map #(find-items rss-item-list %) keyword-list)))
 
-; Find RSS Item by Keywords
-; Find RSS Items by predicate
-; RSS-ITEM-COLL.FindByPredicate(Predicate)
-; (find Predicate RSS-ITEM-COLL)
 
-; return-map-entry <- map.findByValue(key keyword-in-value)
 
 
