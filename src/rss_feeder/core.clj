d5af1a94a5b653b986425035d855b84f5a4846da
Kartik Shah <shahkartikr@gmail.com>
2011-01-23 22:57:44 -0600

    removed folders

--- src/rss_feeder/core.clj
+++
@@ -1,1 +0,0 @@
-(ns rss-feeder.core)
--- test/rss_feeder/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns rss-feeder.test.core
-  (:use [rss-feeder.core] :reload)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
